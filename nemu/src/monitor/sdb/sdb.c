d721e3b0b2e263128020a04ca2b704c693ba764d
101JamesLi <james.li25@beijing101id.com>
2023-06-14 15:47:48 +0800

    finish single step

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -52,6 +52,20 @@ static int cmd_q(char *args) {
   return -1;
 }
 
+static int cmd_si(char *args) {
+  char *arg = strtok(args, " ");
+  int n = 1;
+  
+  if(arg == NULL){
+  	n = 1;
+  	printf("too few arguments\n");
+  }else{
+  	n = atoi(arg);
+  }
+  cpu_exec(n);
+  return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -62,6 +76,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si", "single", cmd_si},
 
   /* TODO: Add more commands */
 
